954f233e8842227e63930eea91f40d5eb6bd85f2
danilmoiseenkov <danil.moiseenkoff@gmail.com>
2022-02-11 00:11:09 +0200

    added sending email, added README.md and requirements.txt

---
+++ README.md
@@ -0,0 +1,11 @@
+### How to run
+```shell
+$ pip install -r requirements.txt
+
+$ python3 allo.py
+$ python3 moyo.py
+$ python3 foxtrot.py
+
+$ python3 compare_prices.py
+$ python3 send_mail.py
+```
--- send_results.py
+++
@@ -1,4 +0,0 @@
-from send_mail import send_mail
-
-send_mail('danil.moiseenkoff@gmail.com', ['dantest387@gmail.com'], 'Compered Prices',
-          'Some text', ['low-price.xlsx', 'other_prices.xlsx'] )
